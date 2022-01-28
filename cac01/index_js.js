843590755dd96c69ba8c53bfcb3b95355916a199
ChristyAlex2147215 <95110353+ChristyAlex2147215@users.noreply.github.com>
2022-01-28 23:18:33 +0530

    28-01-2022

--- cac01/index_js.js
+++ cac01/index_js.js
@@ -1,11 +1,13 @@
 function validate() {
     var name = document.getElementById("username");
-    var phone = document.getElementById("phone");
+    var phone = document.getElementById("Address");
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var password2 = document.getElementById("password2");
+    var checkBox = document.getElementsByClassName
     if (namevalidation(name) & phonevalidation(phone) & emailvalidation(email) & passwordvalidation(password, password2)) {
         alert("welcome you have sign in now");
+
     }
 
 
@@ -74,8 +76,8 @@ function phonevalidation(phone) {
     if (phonevalue.length == 10) {
         setSuccessFor(phone);
         return true;
-    } else if (phonevalue.length != 10) {
-        setErrorFor(phone, "Phone number should have 10 digits ");
+    } else if (phonevalue === "") {
+        setErrorFor(phone, "Address fiels cannot be blank");
     }
 
 }
@@ -124,52 +126,4 @@ function passwordvalidation(password, password2) {
         setErrorFor(password, "password dosent meet the requirements");
         setSuccessFor(password2);
     }
-}
-
-
-//cookie functions to chage 
-window.onload = chbgcolor();
-
-function chbgcolor() {
-    function randomNumber(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    // Function call
-    var randomNumber = (randomNumber(1, 10));
-    console.log(randomNumber);
-    var today = new Date();
-    var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000); // plus 30 days
-
-    setCookie("1", "red");
-    setCookie("2", 'blue');
-    setCookie("3", 'cyon');
-    setCookie("4", 'green');
-    setCookie("5", 'yellow');
-    setCookie("6", 'orange');
-    setCookie("7", 'brickred');
-    setCookie("8", 'white');
-    setCookie("9", 'black');
-    setCookie("10", 'violet');
-
-
-    function getCookie(name) {
-        var re = new RegExp(name + "=([^;]+)");
-        var value = re.exec(document.cookie);
-        return (value != null) ? unescape(value[1]) : null;
-    }
-
-    function setCookie(name, value) {
-        document.cookie = name + "=" + escape(value) + "; path=/; expires=" + expiry.toGMTString();
-    }
-    console.log(getCookie(randomNumber));
-    // console.log(document.body.style.backgroundColor);
-    // console.log(document.getElementsByTagName("body").style.backgroundColor);
-
-    // document.getElementsByTagName("body").style.backgroundColor = getCookie(randomNumber);
-    document.body.style.backgroundColor = String(getCookie(randomNumber));
-    // document.getElementsByTagName("body").style.backgroundColor = "red";
-    document.getElementById("form").style.backgroundColor = String(getCookie(randomNumber));
-
 }
